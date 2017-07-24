1315a5b7183dba22e48f2c86d3eb95a6109f4b99
ahmadou18 <ah.gueye@laposte.net>
2017-07-24 10:17:39 +0200

    feat(create):create article

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -8,7 +8,11 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\BlogPost;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class BlogController extends Controller
 {
@@ -34,7 +38,44 @@ class BlogController extends Controller
     public function createAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('blog/create_blog.html.twig');
+        $blogpost = new BlogPost;
+
+        $form = $this->createFormBuilder($blogpost) //création du formulaire
+        ->add('title', TextType::class, array('label'=> 'Titre', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')) )
+        ->add('body', TextareaType::class, array('label'=> 'Texte', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')) )
+        ->add('save', SubmitType::class, array('label'=> 'Créer un article', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')) )
+
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //récupérer les données
+            $title = $form ['title']->getData();
+            $body = $form ['body']->getData();
+
+            $blogpost->setTitle($title);
+            $blogpost->setbody($body);
+
+            //récupérer l'entité
+            $em = $this->getDoctrine()->getManager();
+
+            //traite les nouvelles entités en base de données
+            $em->persist($blogpost);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Article Ajouté!'
+            );
+
+            return $this->redirectToRoute('home'); //rediriger msg vers une page
+        }
+
+        return $this->render('blog/create_blog.html.twig', array(
+            'form'=>$form->createView()
+        ));
     }
 
 
